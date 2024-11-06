5194a1292a9adbd1470a4c79e09a2e9582f79d39
jlong <codeberg.pumen@simplelogin.com>
2024-11-05 21:10:28 -0600

    add a11y to dropdown; scaffold arrow handler

--- frontend/app/_components/dropdown-menu.tsx
+++ frontend/app/_components/dropdown-menu.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { RemixiconComponentType } from "@remixicon/react";
 import {
   RiCheckboxCircleFill,
@@ -21,17 +21,37 @@ export default function DropdownMenu({
   menuTitle,
   menuItems,
 }: DropdownMenuProps) {
-  const [open, setOpen] = useState(false);
+  const [focused, setFocused] = useState(-1);
   const [selected, setSelected] = useState(-1);
+  const [open, setOpen] = useState(false);
+  //const dropdownRef = useRef<HTMLUListElement | null>(null);
+  //const firstMenuItemRef = useRef<HTMLLIElement | null>(null);
+
   const iconClasses = "text-neutral-900 h-5 w-5";
+
+  //useEffect(() => {}, [open]);
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLLIElement>, id: number) => {
     const SPACE = " ";
-    if (e.key === "Enter" || e.key === SPACE) {
-      setSelected(id);
-    }
-    if (e.key === "Escape") {
-      setOpen(false);
+
+    switch (e.key) {
+      case "Enter":
+      case SPACE:
+        setSelected(id);
+        break;
+      case "Escape":
+        setOpen(false);
+        break;
+      case "ArrowDown":
+        // TODO: set focus
+        e.preventDefault();
+        break;
+      case "ArrowUp":
+        // TODO: set focus
+        e.preventDefault();
+        break;
+      default:
+        break;
     }
   };
 
@@ -44,8 +64,10 @@ export default function DropdownMenu({
       <button
         onClick={() => setOpen(!open)}
         className="flex justify-start items-center self-stretch gap-x-1 px-3 py-2 border border-neutral-200 rounded shadow bg-white focus:outline-none focus:ring-2 focus:ring-indigo-200"
-        aria-expanded={open}
         aria-controls="dropdown-menu"
+        aria-expanded={open}
+        aria-haspopup="menu"
+        id="dropdown-button"
       >
         <span className="text-sm font-medium">{menuTitle}</span>
         {open ? (
@@ -62,6 +84,7 @@ export default function DropdownMenu({
       </button>
       {open && (
         <ul
+          aria-labelledby="dropdown-button"
           className="flex flex-col p-2 gap-y-2 rounded-lg shadow-lg bg-white"
           role="menu"
         >
@@ -69,10 +92,13 @@ export default function DropdownMenu({
             <li
               key={id}
               className="flex justify-start items-center self-stretch p-2 gap-x-3 rounded-lg hover:bg-neutral-50 hover:cursor-pointer focus:outline-none focus:ring-2 focus:ring-indigo-200"
-              onClick={() => handleMenuItemClick(id)}
+              onClick={() => {
+                handleMenuItemClick(id);
+                setOpen(false);
+              }}
               onKeyDown={(e) => handleKeyDown(e, id)}
               role="menuitem"
-              tabIndex={0}
+              tabIndex={focused === id ? 0 : -1}
               value={text.toLowerCase()}
             >
               {Icon && <Icon className={iconClasses} />}
