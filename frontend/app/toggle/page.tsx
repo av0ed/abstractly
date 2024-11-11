70caa594fc52c3e374a7264c612f487cde28a1d6
jlong <codeberg.pumen@simplelogin.com>
2024-11-11 14:40:18 -0600

    update toggle a11y, style, keypress

--- frontend/app/toggle/page.tsx
+++ frontend/app/toggle/page.tsx
@@ -1,5 +1,12 @@
 import Toggle from "../_components/toggle";
 
-export default function TogglePage({}) {
-  return <Toggle name="toggle" />;
+export default function TogglePage() {
+  return (
+    <div className="grid items-center justify-items-center gap-6 grid-cols-2 grid-rows-2">
+      <Toggle name="toggle" size="sm" />
+      <Toggle name="toggle" size="sm" disabled={true} />
+      <Toggle name="toggle" size="md" />
+      <Toggle name="toggle" size="md" disabled={true} />
+    </div>
+  );
 }
